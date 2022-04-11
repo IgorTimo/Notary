45280b39a4653d55ea1e398505aefdc97f7a6e48
Timofey Semenyuk <rustfy@gmail.com>
2022-04-11 22:27:37 +0300

    added private deal, factory

--- ethereum/test/sample-test.js
+++ ethereum/test/sample-test.js
@@ -1,19 +1,72 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { solidity } = require("ethereum-waffle");
+const chai = require("chai");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+chai.use(solidity);
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+const PRICE = 10000;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+describe("Greeter", async function () {
+  let deal, issuer, participant;
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  beforeEach(async () => {
+    const [_deployer, _participant] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    console.log("Deploying contracts with the account:", _deployer.address);
+
+    console.log("Account balance:", (await _deployer.getBalance()).toString());
+
+    const PrivateDeal = await hre.ethers.getContractFactory("PrivateDeal");
+    const privateDeal = await PrivateDeal.deploy(_participant.address, PRICE, 'test', 'test')
+
+    console.log("Greeter deployed to:", privateDeal.address);
+
+    deal = privateDeal;
+    issuer = _deployer;
+    participant = _participant;
+  })
+
+  it("It checks that contract successfully initialized", async function () {
+    expect(await deal.price()).to.equal(PRICE);
+  });
+
+  it("It checks that contract receive value from participants", async function () {
+    const tx = await participant.sendTransaction({
+      to: deal.address,
+      value: ethers.BigNumber.from(PRICE),
+    });
+
+    console.log(await deal.payed());
+    expect(await deal.payed()).to.equal(PRICE);
+  });
+
+
+  it("It checks that contract can approve", async function () {
+    const tx = await participant.sendTransaction({
+      to: deal.address,
+      value: ethers.BigNumber.from(PRICE),
+    });
+
+    const dealParticipant = deal.connect(participant)
+
+    await dealParticipant.approve('test', 'test');
+
+    expect(await dealParticipant.approvedByParticipant()).to.equal(true);
+  });
+
+
+  it("It checks that cannot approve twice", async function () {
+    const tx = await participant.sendTransaction({
+      to: deal.address,
+      value: ethers.BigNumber.from(PRICE),
+    });
+
+    const dealParticipant = deal.connect(participant)
+
+    await dealParticipant.approve('test', 'test');
+
+    expect(dealParticipant.approve('test', 'test'))
+      .to.be.revertedWith('Deal has been approved')
   });
 });
